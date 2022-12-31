4e306a21dc58daacfc8b3ac053aca88cdc984428
AndreLebrao <andledrao@gmail.com>
2022-12-31 18:28:40 -0300

    (AprendendoCSharp) : factorial

--- AprendendoCSharp/ConsoleApp1/Program.cs
+++ AprendendoCSharp/ConsoleApp1/Program.cs
@@ -2,6 +2,15 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int n = 10;
+        if (n == 0)
+            Console.WriteLine(1);
+        int factorial = 1;
+        while (n > 1)
+        {
+            factorial *= n;
+            n--;
+        }
+        Console.WriteLine(factorial);
     }
 }

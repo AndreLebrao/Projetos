c586db541ef0393943ad736019a1abe8c4ddd060
AndreLebrao <andledrao@gmail.com>
2023-01-01 11:07:35 -0300

    (AprendendoCSharp) : basics of OOP

---
+++ AprendendoCSharp/Classes/Conta/ContaCorrente.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes.Conta
+{
+    public class ContaCorrente
+    {
+        public ContaCorrente(int Agencia, string Conta)
+        {
+            this.Agencia = Agencia;
+            this.Conta = Conta;
+            this.Titular = new Titular.Cliente();
+            ContasCriadas++;
+        }
+        public static int ContasCriadas { get; private set; }
+        private int agencia;
+        //property example:
+        public int Agencia
+        {
+            get { return this.agencia; }
+            private set
+            {
+                if (value > 0)
+                    this.agencia = value;
+            }
+        }
+        public string Conta { get; set; }
+        public Titular.Cliente Titular { get; set; }
+        private double saldo;
+        public double Saldo
+        {
+            get
+            {
+                return this.saldo;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    return;
+                }
+                else
+                {
+                    this.saldo = value;
+                }
+            }
+        }
+
+        public void Depositar(double valor)
+        {
+            saldo += valor;
+            Console.WriteLine("Saldo da conta pós-depósito = " + saldo);
+        }
+        public bool Sacar(double valor)
+        {
+            if (saldo < valor)
+                return false;
+            saldo -= valor;
+            return true;
+        }
+    }
+}
---
+++ AprendendoCSharp/Classes/Program.cs
@@ -0,0 +1,25 @@
+﻿using Classes.Conta;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        ContaCorrente conta = new ContaCorrente(13,"1012-0");
+        Console.WriteLine($"Titular da conta: {conta.Titular.Nome}");
+        Console.WriteLine($"Número da conta: {conta.Conta}");
+        Console.WriteLine($"Agência: {conta.Agencia}");
+        Console.WriteLine($"Saldo R$ {String.Format("{0:0.00}", conta.Saldo)}");
+
+        conta.Depositar(500);
+        if (conta.Sacar(300) == true)
+            Console.WriteLine("Saldo da conta pós-saque = " + conta.Saldo);
+        else
+            Console.WriteLine("Saldo insuficiente para saque.");
+        Console.WriteLine($"Quantidade de contas criadas: {ContaCorrente.ContasCriadas}");
+        ContaCorrente conta1 = new ContaCorrente(-12, "1012-1");
+        Console.WriteLine($"Agência: {conta.Agencia}");
+        Console.WriteLine($"Quantidade de contas criadas: {ContaCorrente.ContasCriadas}");
+
+        Console.ReadKey();
+    }
+}
